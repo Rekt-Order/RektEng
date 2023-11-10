1fc72bdae5527caaefceaaec3b27f09ad78dc1f2
Rekt-Order <kasugaidoc@gmail.com>
2023-11-10 14:58:05 +0900

    1st test

---
+++ node-tutorial/zenn_tutorial/todo-app/todoapp/routes/users.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+
+let todos = [];
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Miguytz5!0804',
+  database: 'todo_app'
+});
+
+let todos = [];
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'ToDo App',
+    todos: todos,
+  });
+});
+
+router.post('/', function (req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
+  const todo = req.body.add;
+  connection.query(
+    `insert into tasks (user_id, content) values (1, '${todo}');`,
+    (error, results) => {
+      console.log(error);
+      res.redirect('/');
+    }
+  );
+});
+
+module.exports = router;
